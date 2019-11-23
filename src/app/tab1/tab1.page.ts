f1ff23c723628206388721999e4375e3d30ea527
Lucas <lucasemilio.mf@gmail.com>
2019-11-23 03:09:14 -0300

    .

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -6,7 +6,11 @@ import { Component } from '@angular/core';
   styleUrls: ['tab1.page.scss']
 })
 export class Tab1Page {
-
+  public show = true;
   constructor() {}
+
+  display() {
+    this.show = !this.show;
+  }
 
 }
