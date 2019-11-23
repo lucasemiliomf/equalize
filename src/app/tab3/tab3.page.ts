cf0b75e37c85322f5474bc0dd0be0d6f40399982
Lucas <lucasemilio.mf@gmail.com>
2019-11-23 02:37:41 -0300

    .

--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -6,7 +6,27 @@ import { Component } from '@angular/core';
   styleUrls: ['tab3.page.scss']
 })
 export class Tab3Page {
+  public plants = true;
+  public sews = false;
+  public thrash = false;
 
   constructor() {}
 
+  imagesPlants() {
+    this.plants = true;
+    this.sews = false;
+    this.thrash = false;
+  }
+
+  imagesSew() {
+    this.plants = false;
+    this.sews = true;
+    this.thrash = false;
+  }
+
+  imagesThrash() {
+    this.plants = false;
+    this.sews = false;
+    this.thrash = true;
+  }
 }
